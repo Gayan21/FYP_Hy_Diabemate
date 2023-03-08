c5a5ff6b42b4b30a22a3c5ce44f2d58252eee098
Gayan21 <68651319+Gayan21@users.noreply.github.com>
2023-03-08 23:40:52 +0530

    added profile page

---
+++ Fyp_Hy-Diabemate/hy_diabemate_frontend/lib/Profile.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+// import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(Profile());
+}
+
+class Profile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/fadcrepin.jpg'),
+              ),
+              Text(
+                'Crépin Fadjo',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontFamily: 'Pacifico',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'Flutter Developer'.toUpperCase(),
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.teal.shade100,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              InkWell(
+                child: Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+229 96119149',
+                      style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20,
+                          color: Colors.teal.shade900),
+                    ),
+                  ),
+
+                ),
+                // onTap: (){
+                //   _launchURL('tel:+229 96119149');
+                // }
+              ),
+              InkWell(
+                child: Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title:Text(
+                      'fadcrepin@gmail.com',
+                      style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20,
+                          color: Colors.teal.shade900),
+                    ),
+                  ),
+                ),
+                // onTap: (){
+                //   _launchURL('mailto:fadcrepin@gmail.com?subject=Need Flutter developer&body=Please contact me');
+                // },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
