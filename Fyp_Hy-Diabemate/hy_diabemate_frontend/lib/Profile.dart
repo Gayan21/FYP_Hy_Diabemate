c11953f7550bd2b0c7ab3eade5e2cce0a2af3fd7
Gayan21 <68651319+Gayan21@users.noreply.github.com>
2023-03-10 22:57:46 +0530

    add age field to the profile page

--- Fyp_Hy-Diabemate/hy_diabemate_frontend/lib/Profile.dart
+++ Fyp_Hy-Diabemate/hy_diabemate_frontend/lib/Profile.dart
@@ -28,16 +28,16 @@ class Profile extends StatelessWidget {
                   color: Colors.white,
                 ),
               ),
-              Text(
-                'Flutter Developer'.toUpperCase(),
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontFamily: 'SourceSansPro',
-                  color: Colors.teal.shade100,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.5,
-                ),
-              ),
+              // Text(
+              //   'Flutter Developer'.toUpperCase(),
+              //   style: TextStyle(
+              //     fontSize: 20.0,
+              //     fontFamily: 'SourceSansPro',
+              //     color: Colors.teal.shade100,
+              //     fontWeight: FontWeight.bold,
+              //     letterSpacing: 2.5,
+              //   ),
+              // ),
               SizedBox(
                 height: 20.0,
                 width: 150,
@@ -77,6 +77,27 @@ class Profile extends StatelessWidget {
                     ),
                     title:Text(
                       'fadcrepin@gmail.com',
+                      style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20,
+                          color: Colors.teal.shade900),
+                    ),
+                  ),
+                ),
+                // onTap: (){
+                //   _launchURL('mailto:fadcrepin@gmail.com?subject=Need Flutter developer&body=Please contact me');
+                // },
+              ),
+              InkWell(
+                child: Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.timeline,
+                      color: Colors.teal,
+                    ),
+                    title:Text(
+                      '50',
                       style: TextStyle(
                           fontFamily: 'SourceSansPro',
                           fontSize: 20,
