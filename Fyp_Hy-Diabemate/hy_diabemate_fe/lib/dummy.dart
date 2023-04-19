df47662a9e4223a7423e862fbd7f0841045554c6
Gayan21 <68651319+Gayan21@users.noreply.github.com>
2023-04-19 17:10:05 +0530

    api call changes

--- Fyp_Hy-Diabemate/hy_diabemate_fe/lib/dummy.dart
+++ Fyp_Hy-Diabemate/hy_diabemate_fe/lib/dummy.dart
@@ -30,7 +30,7 @@ class _DiabetesPredictorState extends State<dummy> {
 
   Future<void> _predict() async {
     final response = await http.post(
-      Uri.parse('http://127.0.0.1:5000/predict'),
+      Uri.parse('http://localhost:5000/predict'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
