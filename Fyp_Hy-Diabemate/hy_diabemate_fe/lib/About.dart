6a0ee5eee4717fc7cd3cc0aa5d7337552c59885e
Gayan21 <68651319+Gayan21@users.noreply.github.com>
2023-04-23 21:59:36 +0530

    ui changes

--- Fyp_Hy-Diabemate/hy_diabemate_fe/lib/About.dart
+++ Fyp_Hy-Diabemate/hy_diabemate_fe/lib/About.dart
@@ -1,18 +1,16 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
-// ignore: use_key_in_widget_constructors
 class About extends StatelessWidget {
-   final String prediction;
+  final String prediction;
 
   About({required this.prediction});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       backgroundColor: Color.fromARGB(255, 155, 188, 176),
+      backgroundColor: Color.fromARGB(255, 155, 188, 176),
       appBar: AppBar(
-       backgroundColor: Color.fromARGB(255, 74, 102, 95),
+        backgroundColor: Color.fromARGB(255, 74, 102, 95),
         centerTitle: true,
         title: Text(
           "Hy-Diabemate",
@@ -23,70 +21,100 @@ class About extends StatelessWidget {
           ),
         ),
       ),
-      body: ListView(
-        children: [
-          Container(
-            margin: EdgeInsets.all(
-              MediaQuery.of(context).size.height * 0.05,
-            ),
-            width: MediaQuery.of(context).size.width * 0.2,
-            height: MediaQuery.of(context).size.height * 0.4,
-            decoration: BoxDecoration(
-              color: Colors.black87,
-              border: Border.all(color: Colors.black, width: 2.0),
-               // Image.asset
-              image: DecorationImage(
-                image: new AssetImage("lib/assets/images/result.png"),
-              fit: BoxFit.fill,
-                // image: NetworkImage(
-                //     "https://www.flaticon.com/free-icon/type_9031996"),
+      body: Padding(
+        padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.05),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.green.withOpacity(0.3),
+                      spreadRadius: 2,
+                      blurRadius: 10,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                  image: DecorationImage(
+                    image: AssetImage("lib/assets/images/result.png"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.02),
-            child: Text(
-              "Diabetes Predictor will help you find if you have diabetes or not",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontFamily: "courier new",
-                fontSize: 26.0,
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                padding:
+                    EdgeInsets.all(MediaQuery.of(context).size.width * 0.05),
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(16.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.green.withOpacity(0.3),
+                      spreadRadius: 2,
+                      blurRadius: 10,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Center(
+                  child: Text(
+                    "Hy-DiabMate is an advanced tool that can help you determine your likelihood of having diabetes, empowering you to take control of your health and make informed decisions.",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Castoro Titling",
+                      fontSize: 16.0,
+                      shadows: [
+                        Shadow(
+                          color: Colors.green.withOpacity(0.3),
+                          blurRadius: 10,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
               ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.02),
-            child: Text(
-              "We are basically using Deep learning(DL) model behind the seen for predicting diabetes",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontFamily: "courier new",
-                fontSize: 26.0,
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding:
+                    EdgeInsets.all(MediaQuery.of(context).size.width * 0.02),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 2.0,
+                  ),
+                ),
+                child: Text(
+                  "Your prediction: ${this.prediction == '1' ? 'Positive' : 'Negative'}",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Courier New",
+                    fontSize: 26.0,
+                  ),
+                ),
               ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.all(MediaQuery.of(context).size.width * 0.01),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(
-                Radius.circular(10),
-              ),
-              border: Border.all(
-                color: Colors.black,
-                width: 2.0,
-              ),
-            ),
-            padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.02),
-             child: Text(
-   "Your prediction: ${this.prediction == '1' ? 'Positive' : 'Negative'}",
-    style: TextStyle(
-      fontWeight: FontWeight.bold,
-      fontFamily: "courier new",
-      fontSize: 26.0,
-    ),
-  ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
