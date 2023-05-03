247aa63818b46bacfcbd0de6565c75f402651ba3
Gayan21 <68651319+Gayan21@users.noreply.github.com>
2023-05-04 00:01:06 +0530

    added history changes

--- Fyp_Hy-Diabemate/hy_diabemate_fe/lib/location.dart
+++ Fyp_Hy-Diabemate/hy_diabemate_fe/lib/location.dart
@@ -1,104 +1,108 @@
-// import 'package:flutter/material.dart';
-// import 'package:geolocator/geolocator.dart';
+import 'package:flutter/material.dart';
 
-// class LocationScreen extends StatefulWidget {
-//   @override
-//   _LocationScreenState createState() => _LocationScreenState();
-// }
+class HistoryPage extends StatefulWidget {
+  @override
+  _HistoryPageState createState() => _HistoryPageState();
+}
 
-// class _LocationScreenState extends State<LocationScreen> {
-//   Position? _currentPosition;
-//   final Geolocator geolocator = Geolocator();
+class _HistoryPageState extends State<HistoryPage> {
+  List<Map<String, dynamic>> _historyData = [];
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     _getCurrentLocation();
-//   }
+  @override
+  void initState() {
+    super.initState();
+    // Initialize history data with a single entry for the current date/time
+    _addHistoryData(DateTime.now(), true);
+  }
 
-//   _getCurrentLocation() async {
-//     await geolocator
-//         .getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
-//         .then((Position position) {
-//       setState(() {
-//         _currentPosition = position;
-//       });
-//     }).catchError((e) {
-//       print(e);
-//     });
-//   }
+  void _addHistoryData(DateTime dateTime, bool isPositive) {
+    Map<String, dynamic> newData = {
+      'dateTime': dateTime,
+      'isPositive': isPositive,
+    };
+    setState(() {
+      _historyData.add(newData);
+    });
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Location Demo'),
-//       ),
-//       body: Center(
-//         child: _currentPosition == null
-//             ? CircularProgressIndicator()
-//             : Column(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 crossAxisAlignment: CrossAxisAlignment.center,
-//                 children: <Widget>[
-//                   Text(
-//                     'LATITUDE: ${_currentPosition!.latitude}',
-//                     style: TextStyle(fontSize: 18),
-//                   ),
-//                   SizedBox(
-//                     height: 10,
-//                   ),
-//                   Text(
-//                     'LONGITUDE: ${_currentPosition!.longitude}',
-//                     style: TextStyle(fontSize: 18),
-//                   ),
-//                   SizedBox(
-//                     height: 10,
-//                   ),
-//                   RaisedButton(
-//                     onPressed: () {
-//                       _getAddressFromLatLng();
-//                     },
-//                     child: Text("Get Location"),
-//                   ),
-//                 ],
-//               ),
-//       ),
-//     );
-//   }
-
-//   _getAddressFromLatLng() async {
-//     try {
-//       List<Placemark> placemarks = await geolocator.placemarkFromCoordinates(
-//           _currentPosition!.latitude, _currentPosition!.longitude);
-
-//       Placemark place = placemarks[0];
-
-//       showDialog(
-//         context: context,
-//         builder: (BuildContext context) {
-//           return AlertDialog(
-//             title: Text("Current Location"),
-//             content: Text(place.name +
-//                 ", " +
-//                 place.locality +
-//                 ", " +
-//                 place.postalCode +
-//                 ", " +
-//                 place.country),
-//             actions: <Widget>[
-//               FlatButton(
-//                 child: Text("OK"),
-//                 onPressed: () {
-//                   Navigator.of(context).pop();
-//                 },
-//               ),
-//             ],
-//           );
-//         },
-//       );
-//     } catch (e) {
-//       print(e);
-//     }
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Prediction History'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Prediction History',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 16),
+            Card(
+              color: Colors.grey[200],
+              margin: EdgeInsets.all(8),
+              elevation: 4,
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: DataTable(
+                  columns: [
+                    DataColumn(
+                      label: Text('Date',
+                          style: TextStyle(color: Colors.black)),
+                    ),
+                    DataColumn(
+                      label: Text('Status',
+                          style: TextStyle(color: Colors.black)),
+                    ),
+                  ],
+                  rows: _historyData
+                      .map((data) => DataRow(cells: [
+                            DataCell(Text(
+                                '${data['dateTime'].toString()}',
+                                style: TextStyle(color: Colors.black))),
+                            DataCell(
+                              Row(
+                                children: [
+                                  Icon(
+                                    data['isPositive']
+                                        ? Icons.check_circle
+                                        : Icons.cancel,
+                                    color: data['isPositive']
+                                        ? Colors.green
+                                        : Colors.red,
+                                  ),
+                                  SizedBox(width: 8),
+                                  Text(
+                                      data['isPositive']
+                                          ? 'Positive'
+                                          : 'Negative',
+                                      style: TextStyle(color: Colors.black)),
+                                ],
+                              ),
+                            ),
+                          ]))
+                      .toList(),
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Add a new entry to the history with the current date/time
+                _addHistoryData(DateTime.now(), true);
+              },
+              child: Text('Add New Entry'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
